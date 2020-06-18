22b28e837794901106390e03be94ba71201cc622
unknown <larserlandson5@gmail.com>
2020-06-18 18:09:27 -0500

    Adding Last Name as a second element to function infoLookUpTool

--- learning.object.tool.js
+++ learning.object.tool.js
@@ -1,18 +1,17 @@
 "use strict"
 console.log(data[1]);
 
-let firstName = "UMA";
-let lastName = "Bob";
+// let firstName = "UMA";
+// let lastName = "Bob";
 
 let information = console.log("What information do you want. ie:","");
 
-function infoLookUpTool(firstName) {
+function infoLookUpTool(firstName, lastName) {
     let newData = data.filter(function (el) {
-        console.log(firstName)
-        return (firstName == el.firstName)
+        return (firstName == el.firstName && lastName == el.lastName)
     })
     if (newData.length > 0) {
         console.log(newData[0].eyeColor);
     }
 }
-infoLookUpTool("Uma");
+infoLookUpTool("Uma", "Bob");
