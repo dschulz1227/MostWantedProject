9fa2dd27c0e7176d8986288e3811734f21ab6966
Damon Schulz <damon.schulz@gmail.com>
2020-06-22 21:49:45 -0500

    deleted all comments, gaps, and beautified

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 "use strict"
-// '// lets make sure im linked, Lars typed here
-// // mike is here 
-// /*
-// Build all of your functions for displaying and gathering information below (GUI).
 
-
-// app is the function called to start the entire application
 function app(peopleArray) {
   let searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
   let searchResults;
@@ -15,20 +9,16 @@ function app(peopleArray) {
       mainMenu(searchResults, peopleArray);
       break;
     case 'no':
-      searchResults = searchByTraits(peopleArray);   //closed gap after break where old comment with task was, may need to add more search types
+      searchResults = searchByTraits(peopleArray); 
       mainMenu(searchResults, peopleArray);
       break;
     default:
-      app(peopleArray); // restart app
+      app(peopleArray); 
       break;
   }
 }
-//   // Call the mainMenu function ONLY after you find the SINGLE person you are looking for
-//   mainMenu(searchResults, peopleArray); //results are passed with array to be able to find more information of object
-// }
+
 let personsSpouse = 0
-
-
 
 function infoLookUpTool(person) {
   console.log("First Name: " + person.firstName);
@@ -44,8 +34,6 @@ function infoLookUpTool(person) {
   console.log("Current Spouse: " + person.currentSpouse);
 }
 
-
-//create function that displays parents, spouse, and descendants
 function getPerson(id, peopleArray){
   let person = peopleArray.filter(function(el){
     if(el.id == id){
@@ -56,8 +44,6 @@ function getPerson(id, peopleArray){
   });
   return person[0];
 }
-
-//create function that displays JUST descendants
 
 function getSpouse(person, peopleArray){ 
   if(person.currentSpouse == null){
@@ -83,8 +69,6 @@ function getParents(person, peopleArray){
   }
 }
 
-//create function that collects all ID's that do not belong to person within its object. display info of the owners of the ID's
-
 function getDescendants(person, peopleArray){
  let children = peopleArray.filter(function(el){
    if(el.parents.includes(person.id)){
@@ -99,17 +83,12 @@ for(let i =0; i < children.length; i++){
 }
   return children;
 }  
-  
 
-
-// Menu function to call once you find who you are looking for
 function mainMenu(person, peopleArray) {
-
-//   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of peopleArray. We need peopleArray in order to find descendants and other information that the user may want. */
 
   if (!person) {
     alert("Could not find that individual.");
-    return app(peopleArray); // restart
+    return app(peopleArray); 
   }
 
   let displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
@@ -149,11 +128,9 @@ function searchByName(peopleArray) {
       return false;
     }
   })
-  // TODO: find the person using the name they entered  Michael is doing this.
   return foundPerson[0];
 }
 
-// alerts a list of peopleArray
 function displaypeopleArray(peopleArray) {
   alert(peopleArray.map(function (person) {
     return person.firstName + " " + person.lastName;
@@ -161,15 +138,11 @@ function displaypeopleArray(peopleArray) {
 }
 
 function displayPerson(person) {
-  // print all of the information about a person:
-  // height, weight, age, name, occupation, eye color.
   let personInfo = "First Name: " + person.firstName + "\n";
   personInfo += "Last Name: " + person.lastName + "\n";
-  // TODO: finish getting the rest of the information to display
   alert(personInfo);
 }
 
-// // function that prompts and validates user input
 function promptFor(question, valid) {
   do {
     var response = prompt(question).trim();
@@ -177,20 +150,17 @@ function promptFor(question, valid) {
   return response;
 }
 
-// // helper function to pass into promptFor to validate yes/no answers
 function yesNo(input) {
   return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
 }
 
-// // helper function to pass in as default promptFor validation
 function chars(input) {
-  return true; // default validation only
+  return true; 
 }
 
-//search by weight function
 function searchByWeight(peopleArray) {            
   let weight = promptFor("What is the person's weight?", chars);
-  let foundByWeight = peopleArray.filter(function (person) {              //person's trait must match trait entered in prompt
+  let foundByWeight = peopleArray.filter(function (person) {             
     if (person.weight == weight) {
       return true;
     }
@@ -200,7 +170,7 @@ function searchByWeight(peopleArray) {
   })
   return foundByWeight;
 }
-//search by height function
+
 function searchByHeight(peopleArray) {
   let height = promptFor("What is the person's height?", chars);
   let foundByHeight = peopleArray.filter(function (person) {
@@ -213,7 +183,7 @@ function searchByHeight(peopleArray) {
   })
   return foundByHeight;
 }
-//search by eye color function
+
 function searchByEyeColor(peopleArray) {
   let eyecolor = promptFor("What is the person's eye color?", chars);
   let foundByEyeColor = peopleArray.filter(function (person) {
@@ -226,7 +196,7 @@ function searchByEyeColor(peopleArray) {
   })
   return foundByEyeColor;
 }
-//search by gender function
+
 function searchByGender(peopleArray) {
   let gender = promptFor("What is the person's gender?", chars);
   let foundByGender =  peopleArray.filter(function (person) {
@@ -240,19 +210,14 @@ function searchByGender(peopleArray) {
   return foundByGender;
 }
 
-// //**if you filter down to only one person, they will hold index 0 of people array**
-// //create for loop for more 
-
-//search by all traits function
 function searchByTraits(peopleArray) {
-  let yourChoice = prompt("What trait would you like to search by?","eyecolor, weight, height, or gender");         //choose where to start
-  let askAgain = false;       //ask again = false when there are no more options
-  let arr = peopleArray;       //arr takes spot of peopleArray after each loop with new "array" of names that match search
-  while (arr.length > 1) {    //loop continues until only one name left on array
+  let yourChoice = prompt("What trait would you like to search by?","eyecolor, weight, height, or gender");        
+  let arr = peopleArray;       
+  while (arr.length > 1) {    
     if (askAgain == true) {
-      yourChoice = prompt(arr.length + " Matches Found! What trait would you like to search by for these cases?");   //tells how many search matches were found-what trait to search next
+      yourChoice = prompt(arr.length + " Matches Found! What trait would you like to search by for these cases?");   
     }
-    askAgain = true;        //prompts what trait to search for
+    askAgain = true;        /
     if (yourChoice == "weight" || "Weight") {
       arr = searchByWeight(arr); 
     } else if (yourChoice == "height" || "Height") {
@@ -267,7 +232,7 @@ function searchByTraits(peopleArray) {
     alert("No match found")
     return app(peopleArray)
   }
-  return arr[0];    //returns index 0 of array holding our final search result to our app function
+  return arr[0];   
 }
 
 
